b0718085c0e9b9889da865188478e7b52f9349f5
sothulthorn <thornsothul@gmail.com>
2024-02-09 23:47:15 +0900

    Link Component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <h1>Register</h1>;
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to="/login">Login Page</Link>
+    </div>
+  );
 };
 
 export default Register;
