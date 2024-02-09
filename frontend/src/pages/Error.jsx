dba239027f4626c1f6533a106b5616f4ce36c0dd
sothulthorn <thornsothul@gmail.com>
2024-02-09 23:59:47 +0900

    Error Page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,5 +1,14 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error</h1>
+      <Link to="/">Back Home</Link>
+    </div>
+  );
 };
 
 export default Error;
