2126a01476aabea0f79ba37302150b998cd2a8a2
sothulthorn <thornsothul@gmail.com>
2024-02-10 00:21:25 +0900

    Logo Component

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,13 +1,13 @@
 import Wrapper from '../assets/wrappers/LandingPage';
 import main from '../assets/images/main.svg';
-import logo from '../assets/images/logo.svg';
+import { Logo } from '../components';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
