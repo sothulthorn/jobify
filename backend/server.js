ce09bf19c4819babe56186ebae1a85c347bc62c4
sothulthorn <thornsothul@gmail.com>
2024-02-12 13:33:36 +0900

    Get a single Job

--- backend/server.js
+++ backend/server.js
@@ -44,11 +44,21 @@ app.get('/api/v1/jobs', (req, res) => {
 app.post('/api/v1/jobs', (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
-    res.status(400).json({ message: 'Please provide company or position' });
+    res.status(404).json({ message: 'Please provide company or position' });
   }
   const id = nanoid(10);
   const job = { id, company, position };
   jobs.push(job);
+  res.status(201).json({ job });
+});
+
+// GET A SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    res.status(404).json({ message: `There is no job with ${id}` });
+  }
   res.status(200).json({ job });
 });
 
