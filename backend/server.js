4e0e86675f6d56e11e8de7162c511eea34456f99
sothulthorn <thornsothul@gmail.com>
2024-02-12 13:18:47 +0900

    Get All Jobs

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,22 @@ dotenv.config();
 
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 
 const app = express();
+
+let jobs = [
+  {
+    id: nanoid(),
+    company: 'apple',
+    position: 'front-end',
+  },
+  {
+    id: nanoid(),
+    company: 'google',
+    position: 'backend-end',
+  },
+];
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -19,6 +33,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data receive', data: req.body });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
