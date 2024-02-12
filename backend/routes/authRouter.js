9c50afe580f48c50bda5cddc19bc18af46567695
sothulthorn <thornsothul@gmail.com>
2024-02-12 17:30:03 +0900

    Login user

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { register, login } from '../controller/authController.js';
-import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from '../middleware/validationMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', validateRegisterInput, register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
 
 export default router;
