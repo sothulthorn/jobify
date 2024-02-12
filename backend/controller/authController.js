0289ca49e2e8263147abe1a4f200776200c852ea
sothulthorn <thornsothul@gmail.com>
2024-02-12 17:45:41 +0900

    Http Only Cookie

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import { hashPassword, comparePassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -19,5 +20,15 @@ export const login = async (req, res) => {
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
-  res.send('login route');
+
+  const token = createJWT({ userId: user._id, role: user.role });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  res.status(StatusCodes.OK).json({ message: 'User Logged In' });
 };
