9130ad4f9819b8b39b57037774bb5ce5d7d6b921
sothulthorn <thornsothul@gmail.com>
2024-02-12 15:14:20 +0900

    Get all Jobs and Get single Job

--- backend/controller/jobController.js
+++ backend/controller/jobController.js
@@ -16,20 +16,21 @@ let jobs = [
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
+  const jobs = await JobModel.find({});
   res.status(200).json({ jobs });
 };
 
 // CREATE A JOB
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
-  const job = await JobModel.create('');
+  const job = await JobModel.create({ company, position });
   res.status(201).json({ job });
 };
 
 // GET A SINGLE JOB
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await JobModel.findById(id);
   if (!job) {
     res.status(404).json({ message: `There is no job with ${id}` });
   }
