dfc1955df8f451b59fd5ebf51161825464267e2c
sothulthorn <thornsothul@gmail.com>
2024-02-12 18:14:30 +0900

    Add User to Job Routes

--- backend/controller/jobController.js
+++ backend/controller/jobController.js
@@ -3,12 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 // CREATE A JOB
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
