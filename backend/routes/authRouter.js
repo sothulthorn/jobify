19d3705625d8fb7c800c93be8bfa5c9e16aad88f
sothulthorn <thornsothul@gmail.com>
2024-02-12 17:10:39 +0900

    Validate Register User

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { register, login } from '../controller/authController.js';
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', validateRegisterInput, register);
 router.post('/login', login);
 
 export default router;
