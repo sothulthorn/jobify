f59e4421967e242907bb2d383e13b72ada7cbf8a
sothulthorn <thornsothul@gmail.com>
2024-02-12 18:08:52 +0900

    Verify JWT

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,9 +1,15 @@
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) {
+  if (!token) throw new UnauthenticatedError('authentication invalid');
+
+  try {
+    const { userId, role } = verifyJWT(token);
+    req.user = { userId, role };
+    next();
+  } catch (error) {
     throw new UnauthenticatedError('authentication invalid');
   }
-  next();
 };
