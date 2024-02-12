9ecc55ad0f0b7b9c083b3dd47e5b8f06ca999491
sothulthorn <thornsothul@gmail.com>
2024-02-12 11:40:56 +0900

    Logout Container

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft } from 'react-icons/fa';
-import Logo from './Logo';
+import { Logo, LogoutContainer } from './index';
 import { useDashboardContext } from '../pages/DashboardLayout';
 
 const Navbar = () => {
@@ -16,7 +16,9 @@ const Navbar = () => {
           <Logo />
           <h4 className="logo-text">dashboard</h4>
         </div>
-        <div className="btn-container">toggle/logout</div>
+        <div className="btn-container">
+          <LogoutContainer />
+        </div>
       </div>
     </Wrapper>
   );
