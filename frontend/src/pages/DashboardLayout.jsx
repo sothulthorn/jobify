8666d0a3f404176f41bf2829b4536ce63f2085a6
sothulthorn <thornsothul@gmail.com>
2024-02-13 22:13:38 +0900

    Get current user

--- frontend/src/pages/DashboardLayout.jsx
+++ frontend/src/pages/DashboardLayout.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, SmallSidebar, Navbar } from '../components/index';
 import { createContext, useContext, useState } from 'react';
+import customFetch from '../utils/customFetch';
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
+};
 
 const DashboardContext = createContext();
 
 const DashboardLayout = ({ isDarkThemeEnabled }) => {
-  // Temp
-  const user = { name: 'jame' };
+  const { user } = useLoaderData();
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
@@ -44,7 +53,7 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet />
+              <Outlet context={{ user }} />
             </div>
           </div>
         </main>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,12 +7,13 @@ import { toast } from 'react-toastify';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+
   try {
     await customFetch.post('/auth/login', data);
     toast.success('Login Successfully');
     return redirect('/dashboard');
   } catch (error) {
-    toast.error(error?.data?.message);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
