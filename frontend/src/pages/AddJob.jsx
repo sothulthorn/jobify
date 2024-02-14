22b03711df6cddb094b1d799a88f7c07fd4a353f
sothulthorn <thornsothul@gmail.com>
2024-02-14 20:13:01 +0900

    Select Input

--- frontend/src/pages/AddJob.jsx
+++ frontend/src/pages/AddJob.jsx
@@ -1,4 +1,4 @@
-import { FormRow } from '../components/index';
+import { FormRow, FormRowSelect } from '../components/index';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../backend/utils/constants';
@@ -23,6 +23,18 @@ const AddJob = () => {
             labelText="job location"
             name="jobLocation"
             defaultValue={user.location}
+          />
+          <FormRowSelect
+            labelText="Job Status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText="Job Type"
+            name="jobType"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
           />
           <button
             type="submit"
