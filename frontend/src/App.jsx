32b9af4f30e896bebed8facb9d1b7b0b6dbe15de
sothulthorn <thornsothul@gmail.com>
2024-02-14 21:38:08 +0900

    Edit Job Loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,11 +14,13 @@ import {
   EditJob,
 } from './pages/index';
 
-import { action as registerAction } from './pages/Register';
+import { action, action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { action as addJobAction } from './pages/AddJob';
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -76,6 +78,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- frontend/src/pages/EditJob.jsx
+++ frontend/src/pages/EditJob.jsx
@@ -1,4 +1,27 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../backend/utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error.response.data.msg);
+    return redirect('/dashboard/all-jobs');
+  }
+};
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
+  const { data } = useLoaderData();
+
   return <h1>EditJob</h1>;
 };
 
