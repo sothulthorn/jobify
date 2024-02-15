5ebed7dd494f64e6d77f90148a03510102b8b601
sothulthorn <thornsothul@gmail.com>
2024-02-15 21:58:49 +0900

    Update Cloudinary Setup

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ import path from 'path';
 const app = express();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
