67ef463f9b10498bb85d7e34bf75f4a409225cfc
sothulthorn <thornsothul@gmail.com>
2024-02-16 20:32:59 +0900

    Stats Page Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ import { action as editJobAction } from './pages/EditJob';
 import { action as deleteJobAction } from './pages/DeleteJob';
 import { loader as adminLoader } from './pages/Admin';
 import { action as profileAction } from './pages/Profile';
+import { loader as statsLoader } from './pages/Stats';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -63,7 +64,11 @@ const router = createBrowserRouter([
             element: <AddJob />,
             action: addJobAction,
           },
-          { path: 'stats', element: <Stats /> },
+          {
+            path: 'stats',
+            element: <Stats />,
+            loader: statsLoader,
+          },
           {
             path: 'all-jobs',
             element: <AllJobs />,
