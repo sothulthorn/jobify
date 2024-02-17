410ff4e245df42049db94b4270c11efc1c78e06e
sothulthorn <thornsothul@gmail.com>
2024-02-17 15:29:45 +0900

    update server for deploy

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
@@ -44,20 +46,9 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-const __dirname = path.resolve(); // Set __dirname to current directory
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  // any route that is not api will be redirected to index.html
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
 
 // NOT FOUND MIDDLEWARE
 app.use('*', (req, res) => {
