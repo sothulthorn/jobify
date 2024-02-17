c540a25ee083721b73c17de72f4499f2816f80cd
sothulthorn <thornsothul@gmail.com>
2024-02-17 10:45:47 +0900

    Job Status and Job Type

--- backend/controller/jobController.js
+++ backend/controller/jobController.js
@@ -5,7 +5,7 @@ import day from 'dayjs';
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
-  const { search } = req.query;
+  const { search, jobStatus, jobType } = req.query;
 
   const queryObject = {
     createdBy: req.user.userId,
@@ -16,6 +16,14 @@ export const getAllJobs = async (req, res) => {
       { position: { $regex: search, $options: 'i' } },
       { company: { $regex: search, $options: 'i' } },
     ];
+  }
+
+  if (jobStatus && jobStatus !== 'all') {
+    queryObject.jobStatus = jobStatus;
+  }
+
+  if (jobType && jobType !== 'all') {
+    queryObject.jobType = jobType;
   }
 
   const jobs = await Job.find(queryObject);
