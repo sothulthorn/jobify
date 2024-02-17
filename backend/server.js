8fa30be08f57187ac8b32f2fe5a2b256526d7a8f
sothulthorn <thornsothul@gmail.com>
2024-02-17 15:13:26 +0900

    update deploy script

--- backend/server.js
+++ backend/server.js
@@ -30,31 +30,34 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-// app.use(express.static(path.resolve(__dirname, '../public')));
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(mongoSanitize());
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 // ROUTERS
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-});
+const __dirname = path.resolve(); // Set __dirname to current directory
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 // NOT FOUND MIDDLEWARE
 app.use('*', (req, res) => {
